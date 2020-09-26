772e344f455af2c68a18ba027872b3e132e5a213
kaku3 <kodakaku@gmail.com>
2020-09-26 15:52:04 +0900

    fix: cache url

--- pwa/sw.js
+++ pwa/sw.js
@@ -4,7 +4,9 @@ workbox.loadModule('workbox-strategies');
 
 const precacheController = new workbox.precaching.PrecacheController();
 
-precacheController.addToCacheList([
+console.log('sw', location.href)
+const baseUrl = location.href.replace(/\/pwa\/sw.js$/, '');
+const urls = [
     { url:'/index.html', revision: '0.01' },
     { url:'/css/fonts.css', revision: '0.01' },
     { url:'/css/main.css', revision: '0.01' },
@@ -25,7 +27,13 @@ precacheController.addToCacheList([
     { url:'/js/main.js', revision: '0.01' },
     { url:'/js/sheep.js', revision: '0.01' },
     { url:'/js/smoke.js', revision: '0.01' }
-]);
+].map(o => {
+    o.url = baseUrl + o.url;
+    return o;
+})
+console.log(urls)
+
+precacheController.addToCacheList(urls);
 
 self.addEventListener('install', (event) => {
     event.waitUntil(precacheController.install());
