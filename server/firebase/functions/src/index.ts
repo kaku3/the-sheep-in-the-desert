b997e95444df917626ba1c4b1af2937b4870305a
kaku3 <kodakaku@gmail.com>
2020-10-25 01:00:49 +0900

    feat: functions samples.

---
+++ server/firebase/functions/src/index.ts
@@ -0,0 +1,27 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
+
+const db = admin.firestore()
+
+// 読み込みレコード数に対して課金が発生する？
+// score 登録毎にランキング集計し、1レコードで保持する。
+exports.updateHiScores = functions.firestore
+  .document('scores/{scoreId}')
+  .onWrite((change, context) => {
+
+  const q = db.collection('scores').orderBy('score', 'desc').limit(5);
+  q.get()
+  .then((r) => {
+    const hiScores = r.docs.map(d => d.data())
+    db.collection('hiScores').doc('202010')
+    .set({
+      json: JSON.stringify(hiScores)
+    }).catch((err) => {
+      console.error(err)
+    })
+  }).catch((err) => {
+    console.error(err)
+  })
+})
